34c41c079907b59dad951aacfabb1ec6552d8bb3
tjdn1 <tjdn1@DESKTOP-35CG8GB>
2023-03-22 10:21:47 +0900

    DeliveryManager

---
+++ src/manager/DeliverlyManager.java
@@ -0,0 +1,14 @@
+package manager;
+
+public class DeliverlyManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. menu");
+		System.out.println("2. adress");
+		System.out.println("3. remove");
+		System.out.println("4. order");
+		System.out.println("5. exit");
+
+	}
+
+}
