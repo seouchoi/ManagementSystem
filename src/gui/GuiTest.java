0606e0848b086d53f1a10fe65a468a2c5605abeb
서우 최 <tjdn1080@gmail.com>
2023-06-10 14:14:39 +0900

    2023/06/10

--- src/gui/GuiTest.java
+++ src/gui/GuiTest.java
@@ -2,8 +2,8 @@ package gui;
 
 public class GuiTest {
 	public static void main(String[] args) {
-		//MenuSelection menuSelection = new MenuSelection();
-		//AdressAdder adressAdder = new AdressAdder();
+		MenuSelection menuSelection = new MenuSelection();
+		AdressAdder adressAdder = new AdressAdder();
 		AdressViewer adressViewer = new AdressViewer();
 	}
 }
